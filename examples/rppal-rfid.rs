8b9b49fc63f410716ace1c051554a02fed918e69
Jacob Rosenthal <jacobrosenthal@gmail.com>
2019-03-21 07:52:09 -0700

    hal nb timer

--- examples/rppal-rfid.rs
+++ examples/rppal-rfid.rs
@@ -1,18 +1,56 @@
-use bitbang_wiegand::Wiegand;
-use rppal::gpio::Gpio;
+use bitbang_wiegand::{Read, Wiegand};
+use nb::block;
+use rppal::{gpio::Gpio, hal::Timer};
 
-const DATA0: u8 = 18;
-const DATA1: u8 = 17;
+const HID_DATA0: u8 = 2;
+const HID_DATA1: u8 = 3;
+
+const THREE_ONE_EIGHT_DATA0: u8 = 14;
+const THREE_ONE_EIGHT_DATA1: u8 = 15;
+const THREE_ONE_EIGHT_GND: u8 = 4;
 
 fn main() -> ! {
-    // Retrieve the GPIO pin and configure it as an output.
-    let data0 = Gpio::new().unwrap().get(DATA0).unwrap().into_input();
-    let data1 = Gpio::new().unwrap().get(DATA1).unwrap().into_input();
+    // Retrieve the GPIO pin and configure it as an input.
+    let hid_data0 = Gpio::new()
+        .unwrap()
+        .get(HID_DATA0)
+        .unwrap()
+        .into_input_pullup();
+    let hid_data1 = Gpio::new()
+        .unwrap()
+        .get(HID_DATA1)
+        .unwrap()
+        .into_input_pullup();
 
-    let mut wiegand = Wiegand::new(data0, data1);
+    // Retrieve the GPIO pin and configure it as an input.
+    let three_data0 = Gpio::new()
+        .unwrap()
+        .get(THREE_ONE_EIGHT_DATA0)
+        .unwrap()
+        .into_input_pullup();
+    let three_data1 = Gpio::new()
+        .unwrap()
+        .get(THREE_ONE_EIGHT_DATA1)
+        .unwrap()
+        .into_input_pullup();
+
+    //todo probably not ideal
+    let mut gnd = Gpio::new()
+        .unwrap()
+        .get(THREE_ONE_EIGHT_GND)
+        .unwrap()
+        .into_output();
+    gnd.set_low();
+
+    let mut wiegand1 = Wiegand::new(hid_data0, hid_data1);
+    let mut wiegand2 = Wiegand::new(three_data0, three_data1);
+
+    let timer = Timer::new();
 
     loop {
-        let data = wiegand.read();
-        println!("facility:{} id:{}", data.facility, data.id);
+        let data1 = block!(wiegand1.read(timer)).unwrap();
+        println!("facility:{} id:{}", data1.facility, data1.id);
+        let data2 = block!(wiegand2.read(timer)).unwrap();
+        println!("facility:{} id:{}", data2.facility, data2.id);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #![no_std]
 
+use bitrate::Hertz;
 use embedded_hal::digital::InputPin;
+use embedded_hal::timer::CountDown;
+
+use nb;
 
 pub struct Wiegand<Data0, Data1>
 where
@@ -9,6 +13,12 @@ where
 {
     data0: Data0,
     data1: Data1,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    TimedOut,
+    _Extensible,
 }
 
 pub struct WiegandData {
@@ -33,12 +43,43 @@ where
     pub fn new(data0: Data0, data1: Data1) -> Self {
         Self { data0, data1 }
     }
+}
 
-    pub fn read(&mut self) -> WiegandData {
+pub trait Read<T>
+where
+    T: CountDown,
+{
+    fn read(&mut self, mut timer: T) -> nb::Result<WiegandData, Error>;
+}
+
+//might need +Cancel if this doesnt accept https://github.com/rust-embedded/embedded-hal/issues/106
+impl<Data0, Data1, T, U> Read<T> for Wiegand<Data0, Data1>
+where
+    Data0: InputPin,
+    Data1: InputPin,
+    T: embedded_hal::timer::CountDown<Time = U>,
+    U: From<Hertz<u64>>,
+{
+    fn read(&mut self, mut timer: T) -> nb::Result<WiegandData, Error> {
+        while self.data0.is_high() && self.data1.is_high() {
+            return Err(nb::Error::WouldBlock);
+        }
+
         let mut data_in: u32 = 0;
 
         for _bit in 0..26 {
-            while self.data0.is_high() && self.data1.is_high() {}
+            //more than 100us here would be a problem so 1ms?
+            //were not blocking on timer.wait so if its resolution is less than ms
+            // say us, then this will tick at us until we go high again
+            // or error if it gets up to 1ms
+            timer.start(Hertz(1000));
+            while self.data0.is_high() && self.data1.is_high() {
+                match timer.wait() {
+                    Err(nb::Error::Other(_e)) => unreachable!(),
+                    Err(nb::Error::WouldBlock) => continue,
+                    Ok(()) => return Err(nb::Error::Other(Error::TimedOut)),
+                }
+            }
 
             //shift first because we have room to spare on the left
             //and because we dont want to be shifted over after bit 26
@@ -48,9 +89,20 @@ where
                 data_in |= 1;
             }
 
-            while self.data0.is_low() || self.data1.is_low() {}
+            //more than 1ms here would be a problem so 2ms?
+            //were not blocking on timer.wait so if its resolution is less than ms
+            // say us, then this will tick at us until we go high again
+            // or error if it gets up to 2ms
+            timer.start(Hertz(2000));
+            while self.data0.is_low() || self.data1.is_low() {
+                match timer.wait() {
+                    Err(nb::Error::Other(_e)) => unreachable!(),
+                    Err(nb::Error::WouldBlock) => continue,
+                    Ok(()) => return Err(nb::Error::Other(Error::TimedOut)),
+                }
+            }
         }
 
-        data_in.into()
+        Ok(data_in.into())
     }
 }
