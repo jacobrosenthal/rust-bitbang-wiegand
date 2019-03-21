315dc3d202759cbd506aa9e7fe309fecbf2407c5
Jacob Rosenthal <jacobrosenthal@gmail.com>
2019-03-21 07:52:09 -0700

    rppal example: pull up pins

--- examples/rppal-rfid.rs
+++ examples/rppal-rfid.rs
@@ -6,8 +6,8 @@ const DATA1: u8 = 17;
 
 fn main() -> ! {
     // Retrieve the GPIO pin and configure it as an output.
-    let data0 = Gpio::new().unwrap().get(DATA0).unwrap().into_input();
-    let data1 = Gpio::new().unwrap().get(DATA1).unwrap().into_input();
+    let data0 = Gpio::new().unwrap().get(DATA0).unwrap().into_input_pullup();
+    let data1 = Gpio::new().unwrap().get(DATA1).unwrap().into_input_pullup();
 
     let mut wiegand = Wiegand::new(data0, data1);
 
